928d157bc87579be8b4060839f3009783dd39f50
Molupoju-Bhavana <b20488302@gmail.com>
2025-10-17 10:18:02 +0530

    commit my project

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript here for dynamic functionalities
+// For example, if you wanted to load member data from an API,
+// or add interactive elements.
+// For this static layout, no specific JS is required.
+console.log("Club Overview Page Loaded");
---
+++ script1.js
@@ -0,0 +1,4 @@
+// No specific dynamic functionalities required for this static layout.
+// You could add event listeners here if the "Events" button needed to do something,
+
+    console.log("Clickcadets Club Overview Page Loaded");
